d8a7b2b1a9dd516c1255e35edc10ea36e464831f
TjorvenDM <tjorven.demeulenaere@gmail.com>
2022-07-11 15:34:42 +0200

    werkend ssd voorbeeld import model

--- Testing/Examples/SSD/ObjectDetectionImportSSD/build.gradle.kts
+++ Testing/Examples/SSD/ObjectDetectionImportSSD/build.gradle.kts
@@ -12,6 +12,14 @@ repositories {
 dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
+    implementation("org.slf4j:slf4j-api:1.7.26")
+    implementation("org.slf4j:slf4j-simple:1.7.26")
+    implementation("ai.djl.pytorch:pytorch-native-auto:1.8.1")
+//    implementation("ai.djl.pytorch:pytorch-model-zoo:0.11.0")
+    // https://mavenlibs.com/maven/dependency/ai.djl.mxnet/mxnet-model-zoo
+    implementation("ai.djl.mxnet:mxnet-model-zoo:0.17.0")
+    //   implementation("org.knowm.datasets:datasets-cifar10:2.1.0")
+//    implementation("ai.djl:basicdataset:0.17.0")
 }
 
 tasks.getByName<Test>("test") {
--- Testing/Examples/SSD/ObjectDetectionImportSSD/src/main/java/Main.java
+++ Testing/Examples/SSD/ObjectDetectionImportSSD/src/main/java/Main.java
@@ -1,2 +1,56 @@
+/*
+ * src: //https://www.youtube.com/watch?v=bz4sBv8LTGM
+ * */
+
+import ai.djl.Application;
+import ai.djl.MalformedModelException;
+import ai.djl.inference.Predictor;
+import ai.djl.modality.Classifications;
+import ai.djl.modality.cv.Image;
+import ai.djl.modality.cv.ImageFactory;
+import ai.djl.modality.cv.output.DetectedObjects;
+import ai.djl.repository.zoo.*;
+import ai.djl.training.util.ProgressBar;
+import ai.djl.translate.TranslateException;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class Main {
+    //final layers wegnemen resnet 50 --> boat/no boat
+    public static void main(String[] args) throws IOException, ModelNotFoundException, MalformedModelException, TranslateException {
+        Criteria<Image, DetectedObjects> criteria = Criteria.builder()
+//                .optApplication(Application.CV.OBJECT_DETECTION)
+//                .setTypes(Image.class, DetectedObjects.class)
+////                .optArtifactId("ssd")
+//                .optFilter("backbone", "resnet50")
+//                .optProgress(new ProgressBar())
+//                .build();
+                .optApplication(Application.CV.OBJECT_DETECTION)
+                .setTypes(Image.class, DetectedObjects.class)
+                .optFilter("backbone", "vgg16")
+                .optFilter("dataset", "voc")
+                .optEngine("MXNet")
+                .optProgress(new ProgressBar())
+                .build();
+
+        Path img_path = Paths.get("images/737200_2.jpg");
+        Image img = ImageFactory.getInstance().fromFile(img_path);
+
+        //load image
+        ZooModel<Image, DetectedObjects> model = ModelZoo.loadModel(criteria);
+
+        //predict model
+        Predictor<Image, DetectedObjects> predictor = model.newPredictor();
+        DetectedObjects detected_objects = predictor.predict(img);
+
+        model.close();
+        System.out.println(detected_objects);
+
+        img.drawBoundingBoxes(detected_objects);
+        Path resultPath = Paths.get("results/result.png");
+        img.save(Files.newOutputStream(resultPath), "png");
+    }
 }
