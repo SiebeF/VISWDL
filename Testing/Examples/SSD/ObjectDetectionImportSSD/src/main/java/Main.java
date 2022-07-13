858f0ed7a7720adacdae3712c07dc522092b28ad
TjorvenDM <tjorven.demeulenaere@gmail.com>
2022-07-13 11:06:18 +0200

    add filename to csv

--- Testing/Examples/SSD/ObjectDetectionImportSSD/src/main/java/Main.java
+++ Testing/Examples/SSD/ObjectDetectionImportSSD/src/main/java/Main.java
@@ -8,7 +8,9 @@ import ai.djl.inference.Predictor;
 import ai.djl.modality.Classifications;
 import ai.djl.modality.cv.Image;
 import ai.djl.modality.cv.ImageFactory;
+import ai.djl.modality.cv.output.BoundingBox;
 import ai.djl.modality.cv.output.DetectedObjects;
+import ai.djl.modality.cv.output.Rectangle;
 import ai.djl.nn.Blocks;
 import ai.djl.nn.SequentialBlock;
 import ai.djl.nn.SymbolBlock;
@@ -35,7 +37,7 @@ public class Main {
     //final layers wegnemen resnet 50 --> boat/no boat
     public static void main(String[] args) throws IOException, ModelNotFoundException, MalformedModelException, TranslateException {
         File[] files = new File("images").listFiles();
-        getDirectory(files);
+        runApplication(files);
     }
 
     private static Criteria<Image, DetectedObjects> getCriteria(){
@@ -55,14 +57,14 @@ public class Main {
     }
 
     //put important data in csv file
-    private static void toCSV(double mAP, int imageWidth, int imageHeight, String className) throws IOException {
+    private static void toCSV(String filename, double mAP, int imageWidth, int imageHeight, String className) throws IOException {
         File file = new File("results/results.csv");
 
         try{
             FileWriter outputfile = new FileWriter(file, true); //true: file niet overwriten, toevoegen
             CSVWriter writer = new CSVWriter(outputfile);
             List<String[]> data = new ArrayList<String[]>();
-            data.add(new String[] {String.valueOf(mAP), String.valueOf(imageWidth), String.valueOf(imageWidth), className});
+            data.add(new String[] {filename, String.valueOf(mAP), String.valueOf(imageWidth), String.valueOf(imageWidth), className});
             writer.writeAll(data);
 
             writer.close();
@@ -85,30 +87,32 @@ public class Main {
         System.out.println(detected_objects);
 
         img.drawBoundingBoxes(detected_objects);
+
         String nameWithoutExtension = FilenameUtils.removeExtension(imageName);
         Path resultPath = Paths.get("results/result_"+ nameWithoutExtension+".png");
         img.save(Files.newOutputStream(resultPath), "png");
-
+        Object image_obj = img.getWrappedImage();
+        System.out.println(image_obj);
         //save data to csv
         try {
             double mAP = detected_objects.best().getProbability();
             String className = detected_objects.best().getClassName();
             int imageSize = img.getHeight() * img.getWidth();
-            toCSV(mAP, img.getWidth(), img.getHeight(), className);
+            toCSV(nameWithoutExtension,mAP, img.getWidth(), img.getHeight(), className);
         }
         catch(final Exception e){
             e.printStackTrace();
-            toCSV(0,0,0,null);
+            toCSV(nameWithoutExtension,0,0,0,null);
         }
     }
 
     //create a list of all images
-    private static void getDirectory(File[] files) throws IOException, TranslateException, ModelNotFoundException, MalformedModelException {
+    private static void runApplication(File[] files) throws IOException, TranslateException, ModelNotFoundException, MalformedModelException {
         //src: https://www.geeksforgeeks.org/java-program-to-traverse-in-a-directory/
 
         for (File filename : files) {
             if (filename.isDirectory()) {
-                getDirectory(Objects.requireNonNull(filename.listFiles()));
+                runApplication(Objects.requireNonNull(filename.listFiles()));
             } else if(FileNameUtils.getExtension(filename.getName()).equals("jpg")){ //make sure file is a jpg image
                 Path img_path = Paths.get(filename.getPath());
                 Image img = ImageFactory.getInstance().fromFile(img_path);
